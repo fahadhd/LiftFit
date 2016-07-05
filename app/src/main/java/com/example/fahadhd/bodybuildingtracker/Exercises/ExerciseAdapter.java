e48de033c1293ca6c1d7695bae12ea5188245095
Fahad <fha423@gmail.com>
2016-07-05 12:00:35 -0400

    Exercise_List_Item_XML: Add placeholder views for a single workout in list

---
+++ app/src/main/java/com/example/fahadhd/bodybuildingtracker/Exercises/ExerciseAdapter.java
@@ -0,0 +1,11 @@
+package com.example.fahadhd.bodybuildingtracker.Exercises;
+
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+public class ExerciseAdapter {
+    Context context;
+    ArrayList<Workout> workouts;
+}
---
+++ app/src/main/java/com/example/fahadhd/bodybuildingtracker/Exercises/ExerciseLoader.java
@@ -0,0 +1,36 @@
+package com.example.fahadhd.bodybuildingtracker.Exercises;
+
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.util.List;
+
+public class ExerciseLoader extends AsyncTaskLoader<List<Workout>> {
+    List<Workout> cachedData;
+
+    public ExerciseLoader(Context context){
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if(cachedData == null){
+            forceLoad();
+        }
+        else{
+            super.deliverResult(cachedData);
+        }
+    }
+
+    @Override
+    public List<Workout> loadInBackground() {
+        return null;
+    }
+
+    @Override
+    public void deliverResult(List<Workout> data) {
+        cachedData = data;
+        super.deliverResult(cachedData);
+    }
+}
